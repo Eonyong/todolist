05c7aeddab84e695274590dc43966aa7796b48e3
정언용 <unjoo94@naver.com>
2022-11-29 16:38:44 +0900

    Home.js edit

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.listen(8080, () => {
 })
 
 app.use(express.json)
-var cors = require('cors')
-app.use(cors())
+// var cors = require('cors')
+// app.use(cors())
 
 // 아래 코드를 작성해야 project/build에 있는 static 파일들을 전송해줄 수 있다.
 app.use(express.static(path.join(__dirname, 'todo/build')))
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 // readt-router를 사용하는 경우 아래의 코드를 최하단에 작성해놓는 것이 좋다.
 // 이는 리액트에서 라우팅을 작성하는 경우 서버에서의 에러를 방지할 수 있가 때문
 app.get('*' , (req, res) => {
-  res.sendFile(path.join(__dirname, 'todo/build/index.html'))
+  res.sendFile(path.join(__dirname, '/todo/build/index.html'))
 })
