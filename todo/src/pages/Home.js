98e096ac4dc699ae94f6d4b98b125b7543798e41
정언용 <unjoo94@naver.com>
2022-11-29 14:58:55 +0900

    Home.js 작성

--- todo/src/pages/Home.js
+++ todo/src/pages/Home.js
@@ -3,14 +3,14 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import Menu from '@mui/icons-material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
 
 const Home = () => {
   return(
     <AppBar position="static" color="transparent">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
-          <Menu />
+          <MenuIcon />
         </IconButton>
         <Typography variant="h6">
           ToDoList
