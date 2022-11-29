04521e6281bba33d22d32ed6e3a280924a39b118
정언용 <unjoo94@naver.com>
2022-11-29 14:38:09 +0900

    navber create

--- todo/pages/Home.js
+++ todo/pages/Home.js
@@ -1,5 +1,23 @@
-const Home = () => {
+import * as React from "react";
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/icons-material/Menu'
 
+const Home = () => {
+  return(
+    <AppBar position="static" color="transparent">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <Menu />
+        </IconButton>
+        <Typography variant="h6">
+          ToDoList
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Home;
