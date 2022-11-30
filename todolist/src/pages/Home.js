cbf72d5e9f2e00aa8df203d1faea65c58b11ce7f
정언용 <unjoo94@naver.com>
2022-11-30 15:27:33 +0900

    create: Home.js

---
+++ todolist/src/pages/Home.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/icons-material/Menu'
+
+
+const Home = () => {
+  return (
+    <AppBar position="static" color="transparent">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <Menu />
+        </IconButton>
+        <Typography variant="h6">
+          Hi
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Home;
