18374844ecbe7bf01346436b83ac9355a784e1f4
정언용 <unjoo94@naver.com>
2022-12-05 16:07:43 +0900

    created: CRUD cod

--- todo_server/models/User.js
+++ todo_server/models/User.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  userid: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   pw: { type: String, required: true },
   data: Object
 })
 
-userSchema.statics.create = function (payload) {
+userSchema.statics.create = (payload) => {
   const user = new this(payload)
   return user.save()
 }
 
+userSchema.statics.findAll = () => this.find({})
+
+userSchema.statics.findOneByUserid = (userid) => this.findOne({ userid })
+
+userSchema.statics.updateByUserid = (userid, payload) => this.findOneAndUpdate({ userid }, payload, { new: true })
+
+userSchema.statics.deleteByUserid = (userid) => this.remove({ userid })
 
 module.exports = mongoose.model("User", userSchema)
